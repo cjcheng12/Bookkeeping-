d6fbc6c6eb78a65874f13b409ae61870990833c2
cjcheng12 <cjcheng12@gmail.com>
2025-12-31 14:04:17 -0500

    Update and rename Home.py to app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import streamlit as st
+from db import init_db
 
 st.set_page_config(page_title="現金流記帳", layout="wide")
+init_db()
 
 st.title("現金流記帳程式")
-
-account = st.selectbox(
-    "你要記錄哪一個帳戶呢？",
-    ["常弘服裝", "個人開銷"]
-)
-
+account = st.selectbox("你要記錄哪一個帳戶呢？", ["常弘服裝", "個人開銷"])
 st.session_state["account"] = account
 
 st.success(f"目前帳戶：{account}")
